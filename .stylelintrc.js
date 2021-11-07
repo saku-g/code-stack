c739531c638e1691747eb9e7f5457bc0e6a9111f
sakucode <yuusaku.hori.itz@gmail.com>
2021-11-07 18:33:40 +0900

    add: configファイルを追加

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['stylelint-config-recommended-scss', 'stylelint-prettier/recommended'],
+  rules: {
+    'prettier/prettier': [true, { singleQuote: true }],
+    'string-quotes': 'single',
+    'color-hex-length': 'short',
+    'unknown-word': null,
+  },
+};

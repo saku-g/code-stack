4db87da25b37985349a035a32b957911c23306df
sakucode <sakucode1993@gmail.com>
2021-11-25 23:48:00 +0900

    add: tocFixed.jsを新規作成。Intersection Observerを使用。

--- src/js/tocFixed.js
+++ src/js/tocFixed.js
@@ -1,22 +1,25 @@
 /* ----------------------------------------
   目次固定
+  - headerが見えなくなったら、目次固定
 ---------------------------------------- */
 
 export function tocFixed() {
   const header = document.querySelector('.l-header');
+  const toc = document.querySelector('.post__toc');
 
-  //rootMarginを決めるために高さを取得
-  const headerHeight = header.clientHeight;
+  const callback = (entries) => {
+    const [entry] = entries; //entries[0]と同じ意味
+    if (!entry.isIntersecting) {
+      toc.classList.add('is-fixed');
+    } else {
+      toc.classList.remove('is-fixed');
+    }
+  };
 
   const options = {
     root: null,
     rootMargin: '0px',
     threshold: 0,
-  };
-
-  const callback = function () {
-    // target.classList.add('is-fixed');
-    console.log('test');
   };
 
   const observer = new IntersectionObserver(callback, options);

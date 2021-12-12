05ece0165932e2a5fc3e08c6a20d7dc1ec5c485f
sakucode <sakucode1993@gmail.com>
2021-12-12 21:33:51 +0900

    Add: header.phpとfooter.phpに要素を追加

--- footer.php
+++ footer.php
@@ -1,3 +1,23 @@
-    <?php wp_footer(); ?>
+<?php
+/**
+ * フッターテンプレート
+ *
+ * @package WordPress
+ * @author Sacck
+ */
+
+?>
+
+		<!-- footer-->
+		<footer class="l-footer">
+			<div class="l-footer__copy">
+				<small>© 2021 SACCK.</small>
+			</div>
+		</footer>
+
+	</div><!-- /.l-wrapper -->
+
+<?php wp_footer(); ?>
+
 </body>
 </html>

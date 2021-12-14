64f93d8c5b3482214235b80c42bf16f35f52669b
sakucode <sakucode1993@gmail.com>
2021-12-14 23:15:32 +0900

    Fix: 関数ファイル名を修正

--- functions.php
+++ functions.php
@@ -10,4 +10,4 @@
  * 関数群の読み込み
  * require_onceを先頭に付け、重複ファイルの読み込み防ぐ
  */
-require_once get_theme_file_path( './functions/add_theme_files.php' ); /* CSS・JSファイルの読み込み */
+require_once get_theme_file_path( './functions/add-theme-file.php' ); /* CSS・JSファイルの読み込み */

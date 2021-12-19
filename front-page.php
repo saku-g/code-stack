794b40cd1847139f25a688b06f9434703badd0b5
sakucode <sakucode1993@gmail.com>
2021-12-19 13:24:55 +0900

    Add: loop.phpを新規作成。記事ループ部分を別ファイルとして管理。

--- front-page.php
+++ front-page.php
@@ -21,29 +21,9 @@
 		</div>
 	<?php if ( have_posts() ) : ?>
 		<div class="cards">
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-			<article class="card">
-				<a class="card__link" href="<?php the_permalink(); ?>">
-					<div class="card__body">
-						<h3 class="heading card__title"><?php the_title(); ?></h3>
-					</div>
-					<div class="card__thumbnail">
-						<?php
-						if ( has_post_thumbnail() ) :
-							the_post_thumbnail( 'list-thumbnail' );
-							?>
-						<?php else : ?>
-							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/default-thumbnail.png" width="280" height="156" srcset="">
-						<?php endif; ?>
-					</div>
-				</a>
-			</article>
-		<?php endwhile; ?>
-	<?php endif; ?>
+			<?php get_template_part( 'template-parts/loop' ); ?>
 		</div>
+	<?php endif; ?>
 	</div><!-- /.l-container -->
 </main>
 

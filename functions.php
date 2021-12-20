0604e2aa7c23b03e50abf34cf26a2d084c5d7daf
sakucode <sakucode1993@gmail.com>
2021-12-20 23:18:15 +0900

    Add: 管理画面上からlazyblockを追加。alertとrelated-article。
    親要素のdivを生成しないようfunctionsに設定

--- functions.php
+++ functions.php
@@ -22,3 +22,4 @@ add_action( 'after_setup_theme', 'my_theme_setup' );
 require_once get_theme_file_path( './functions/add-theme-files.php' ); /* CSS・JSファイルの読み込み */
 require_once get_theme_file_path( './functions/add-image-size.php' ); /* サムネイルサイズの設定 */
 require_once get_theme_file_path( './functions/search-filter.php' ); /* 検索対象を投稿のみに適用 */
+require_once get_theme_file_path( './functions/remove-lazyblocks-div.php' ); /* Lazy Blocksで自動付与される親要素のdivを取り除く */

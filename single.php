24ee916a81b9003f91e6638d282d6011ec04599c
sakucode <sakucode1993@gmail.com>
2021-12-26 19:42:58 +0900

    Add:　記事リンクをショートコードで実装する関数を追加

--- single.php
+++ single.php
@@ -24,25 +24,25 @@
 		<article class="post" id="post">
 			<header class="post__header">
 				<h1 class="heading heading--1"><?php the_title(); ?></h1>
-			</header>
-			<div class="post__meta">
-				<div class="post__date date">
-					<time class="date__posted" itemprop="datePublished" datetime="<?php the_time( 'c' ); ?>">
-						<svg width="13" height="16">
-							<use xlink:href="#calendar" fill="currentColor"></use>
-						</svg>
-						<?php the_time( 'Y.m.d' ); ?>
-					</time>
-				<?php if ( get_the_time( 'Ymd' ) !== get_the_modified_date( 'Ymd' ) ) : ?>
-					<time class="date__updated" itemprop="dateModified" datetime="<?php the_modified_date( 'c' ); ?>">
-						<svg width="16" height="16">
-							<use xlink:href="#update" fill="currentColor"></use>
-						</svg>
-						<?php the_modified_time( 'Y.m.d' ); ?>
-					</time>
-				<?php endif; ?>
+				<div class="post__meta">
+					<div class="post__date date">
+						<time class="date__posted" itemprop="datePublished" datetime="<?php the_time( 'c' ); ?>">
+							<svg width="13" height="16">
+								<use xlink:href="#calendar" fill="currentColor"></use>
+							</svg>
+							<?php the_time( 'Y.m.d' ); ?>
+						</time>
+					<?php if ( get_the_time( 'Ymd' ) !== get_the_modified_date( 'Ymd' ) ) : ?>
+						<time class="date__updated" itemprop="dateModified" datetime="<?php the_modified_date( 'c' ); ?>">
+							<svg width="16" height="16">
+								<use xlink:href="#update" fill="currentColor"></use>
+							</svg>
+							<?php the_modified_time( 'Y.m.d' ); ?>
+						</time>
+					<?php endif; ?>
+					</div>
 				</div>
-			</div>
+			</header>
 			<?php /* 目次がある場合はここに差し込まれる。目次はJSで生成。  */ ?>
 			<section class="post__body edit-area">
 				<?php
